65af97e6ea468b72f4da49d8e64927965d84815b
Jean Osco <willjean29@gmail.com>
2024-10-15 17:33:42 -0500

    feat(primeapp): improve main with gotuines and channels

--- primeapp/main.go
+++ primeapp/main.go
@@ -1,11 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	n := 0
-	_, msg := isPrime(n)
-	fmt.Println(msg)
+	// print welcome message
+	intro()
+
+	// create a channel to communicate with the readUserInput goroutine
+	doneChan := make(chan bool)
+
+	// start the readUserInput goroutine
+	go readUserInput(doneChan)
+
+	// wait for the readUserInput goroutine to finish
+	<-doneChan
+
+	// close the doneChan channel
+	close(doneChan)
+
+	// print goodbye message
+	fmt.Println("Goodbye!")
+
+}
+
+func intro() {
+	fmt.Println("Welcome to the prime number app!")
+	fmt.Println("This app will tell you if a number is prime or not.")
+	fmt.Println("Let's get started!")
+	prompt()
+}
+
+func prompt() {
+	fmt.Println("->")
+}
+
+func readUserInput(doneChan chan bool) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		res, done := checkNumbers(scanner)
+
+		if done {
+			doneChan <- true
+			return
+		}
+		fmt.Println(res)
+		prompt()
+	}
+}
+
+func checkNumbers(scanner *bufio.Scanner) (string, bool) {
+	// read user input
+	scanner.Scan()
+	// check if user wants to quit
+	if scanner.Text() == "q" {
+		return "", true
+	}
+
+	//  try to convert user input to an integer
+	numToCheck, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		return "Please make a whole number!", false
+	}
+
+	// check if the number is prime
+	_, msg := isPrime(numToCheck)
+
+	return msg, false
 }
 
 func isPrime(n int) (bool, string) {
