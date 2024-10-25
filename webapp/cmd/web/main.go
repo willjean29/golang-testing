ed7c5c8aba796fd3a16a8e3f9d8875b96ebbeab3
Jean Osco <willjean29@gmail.com>
2024-10-24 21:45:39 -0500

    feat(webapp): add connection to pg

--- webapp/cmd/web/main.go
+++ webapp/cmd/web/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"flag"
 	"log"
 	"net/http"
 
@@ -10,16 +11,29 @@ import (
 
 type application struct {
 	Session *scs.SessionManager
+	DSN     string
+	DB      *sql.DB
 }
 
 func main() {
 	app := &application{}
 
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=users ", "Postgres connection")
+	flag.Parse()
+
+	conn, err := app.connectToDB()
+
+	if err != nil {
+		log.Fatal("Error connecting to database: " + err.Error())
+	}
+
+	app.DB = conn
+
 	app.Session = getSession()
 
-	fmt.Println("Starting server on :8080")
+	log.Println("Starting server on :8080")
 
-	err := http.ListenAndServe("localhost:8080", app.routes())
+	err = http.ListenAndServe("localhost:8080", app.routes())
 
 	if err != nil {
 		log.Fatal("Error starting server: " + err.Error())
--- webapp/go.mod
+++ webapp/go.mod
@@ -4,4 +4,17 @@ go 1.22.2
 
 require github.com/go-chi/chi/v5 v5.1.0
 
-require github.com/alexedwards/scs/v2 v2.8.0 
+require github.com/alexedwards/scs/v2 v2.8.0
+
+require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/pgx/v4 v4.18.3 // indirect
+	golang.org/x/crypto v0.20.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
