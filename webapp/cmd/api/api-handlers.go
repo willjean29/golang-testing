d3eb5175bbfde7f157c188357c0f17092fadb699
Jean Osco <willjean29@gmail.com>
2024-11-07 12:19:13 -0500

    feat(webapp-api): add refresk token handler

--- webapp/cmd/api/api-handlers.go
+++ webapp/cmd/api/api-handlers.go
@@ -3,7 +3,10 @@ package main
 import (
 	"errors"
 	"net/http"
+	"strconv"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -44,7 +47,61 @@ func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) refresh(w http.ResponseWriter, r *http.Request) {
-	// Refresh user
+	err := r.ParseForm()
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	refreshToken := r.Form.Get("refresh_token")
+	claims := &Clams{}
+
+	_, err = jwt.ParseWithClaims(refreshToken, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(app.JWTSecret), nil
+	})
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if time.Unix(claims.ExpiresAt.Unix(), 0).Sub(time.Now()) > 30*time.Second {
+		app.errorJSON(w, errors.New("refresh token is expired"), http.StatusBadRequest)
+		return
+	}
+
+	userId, err := strconv.Atoi(claims.Subject)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	user, err := app.DB.GetUser(userId)
+
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	tokenPairs, err := app.generateTokenPair(user)
+	if err != nil {
+		app.errorJSON(w, errors.New("unknown user"), http.StatusBadRequest)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "_Host-refresh_token",
+		Path:     "/",
+		Value:    tokenPairs.RefreshToken,
+		Expires:  time.Now().Add(jwtRefreshTokenExpiry),
+		MaxAge:   int(jwtRefreshTokenExpiry.Seconds()),
+		SameSite: http.SameSiteStrictMode,
+		Domain:   "localhost",
+		Secure:   true,
+		HttpOnly: true,
+	})
+
+	_ = app.writeJSON(w, http.StatusOK, tokenPairs)
 }
 
 func (app *application) allUsers(w http.ResponseWriter, r *http.Request) {
