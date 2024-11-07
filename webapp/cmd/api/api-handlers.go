a13c92c92bbdcf1105720bb29cb1625f67a3f726
Jean Osco <willjean29@gmail.com>
2024-11-07 15:56:33 -0500

    feat(webapp-api): complete others handler

--- webapp/cmd/api/api-handlers.go
+++ webapp/cmd/api/api-handlers.go
@@ -5,7 +5,9 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+	"webapp/pkg/data"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -105,21 +107,79 @@ func (app *application) refresh(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) allUsers(w http.ResponseWriter, r *http.Request) {
-	// All users
+	users, err := app.DB.AllUsers()
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, users)
 }
 
 func (app *application) getUser(w http.ResponseWriter, r *http.Request) {
-	// Get user
+	userId, err := strconv.Atoi(chi.URLParam(r, "userId"))
+	if err != nil {
+		app.errorJSON(w, errors.New("invalid user id"), http.StatusBadRequest)
+		return
+	}
+
+	user, err := app.DB.GetUser(userId)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, user)
 }
 
 func (app *application) updateUser(w http.ResponseWriter, r *http.Request) {
-	// Update user
+	var user data.User
+	err := app.readJSON(w, r, &user)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	err = app.DB.UpdateUser(user)
+
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
 
 func (app *application) deleteUser(w http.ResponseWriter, r *http.Request) {
-	// Delete user
+	userId, err := strconv.Atoi(chi.URLParam(r, "userId"))
+	if err != nil {
+		app.errorJSON(w, errors.New("invalid user id"), http.StatusBadRequest)
+		return
+	}
+
+	err = app.DB.DeleteUser(userId)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func (app *application) insertUser(w http.ResponseWriter, r *http.Request) {
-	// Insert user
+	var user data.User
+	err := app.readJSON(w, r, &user)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	_, err = app.DB.InsertUser(user)
+
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
